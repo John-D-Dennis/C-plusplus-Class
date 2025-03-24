25dc7384fd2ead63f54a26d621635967de8cfb8f
John-D-Dennis <Jddennis1992@gmail.com>
2025-03-23 22:48:13 -0400

    Add files via upload

---
+++ List_making.cxx
@@ -0,0 +1,16 @@
+// Making Lists or moving text to the next line under it
+
+#include <iostream>
+using namespace std;
+
+int main () {
+	
+	cout << "Testing app" <<endl;
+	
+	cout << "Lists" << endl;
+	cout << "Easier to use shorter way\n";
+	cout << "to move one line under.\a "
+	              "It's simpler";
+	
+	return 0;
+}
