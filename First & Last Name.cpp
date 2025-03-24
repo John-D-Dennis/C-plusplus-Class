19fcf54b82b5e240fc08def35dcd603d78c63c86
John-D-Dennis <Jddennis1992@gmail.com>
2025-03-23 22:31:32 -0400

    Add files via upload

---
+++ First & Last Name.cpp
@@ -0,0 +1,22 @@
+// Online Class Assignemnt (FIRST, LAST NAME)
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	 cout << "My name is\a";
+     cout << " John Dennis" <<endl; // endl ends the text on the current line. The next one will be on the next line under in the command prompt.
+	 cout << " " << endl;
+
+	 cout << "I am learning to code" <<endl;
+	 cout << " " << endl;
+
+	 cout << "I've also seen how to make lists like: " << endl; // this is a lower case L, not a one (1)
+	 cout << "this\n";
+	 cout << "to this\n"; // \n is another way to end the line, but i dont like it.
+	 cout << "and this" << endl;
+
+
+		return 0; //Remember not to make this [ return = 0 ]. It will come up as an error
+}
